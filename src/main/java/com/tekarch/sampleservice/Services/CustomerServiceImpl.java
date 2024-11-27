b3391dcdf20128ab29021c64abfafb35f8f181b4
shruthi-r18 <shruthiramachandra162@gmail.com>
2024-11-26 16:40:37 -0800

    Added logging

--- src/main/java/com/tekarch/sampleservice/Services/CustomerServiceImpl.java
+++ src/main/java/com/tekarch/sampleservice/Services/CustomerServiceImpl.java
@@ -3,6 +3,8 @@ package com.tekarch.sampleservice.Services;
 import com.tekarch.sampleservice.Models.Customer;
 import com.tekarch.sampleservice.Repositories.CustomerRepository;
 import com.tekarch.sampleservice.Services.Interfaces.CustomerService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,18 +13,21 @@ import java.util.List;
 public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
-
+    private static final Logger logger = LogManager.getLogger(CustomerServiceImpl.class);
     public CustomerServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
     @Override
     public List<Customer> getAllCustomer() {
+        logger.info("Display all customer");
         return customerRepository.findAll();
+
     }
 
     @Override
     public Customer addCustomer(Customer customer) {
+        logger.info("add new  customer");
         return customerRepository.save(customer);
     }
 }
